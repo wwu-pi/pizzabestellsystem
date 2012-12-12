4573e6d02abe0c95bb0a91b407805d16c2a4372d
Jan Ernsting <jan.ernsting@gmail.com>
2012-12-12 13:04:38 +0100

    Change time comparator into final class

--- src/de/herrbert/pizza/domain/BestellungZeitComparator.java
+++ src/de/herrbert/pizza/domain/BestellungZeitComparator.java
@@ -2,7 +2,7 @@ package de.herrbert.pizza.domain;
 
 import java.util.Comparator;
 
-public class BestellungZeitComparator implements Comparator<Bestellung> {
+public final class BestellungZeitComparator implements Comparator<Bestellung> {
 	@Override
 	public int compare(Bestellung o1, Bestellung o2) {
 		return o2.getZeit().compareTo(o1.getZeit());
