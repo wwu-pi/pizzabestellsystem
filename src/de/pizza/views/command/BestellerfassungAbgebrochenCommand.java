162ef94a343f5d26f5ad12b6198c8fcb24fbfdb4
Jan Ernsting <jan.ernsting@gmail.com>
2013-01-11 09:35:03 +0100

    Extend equivalent class instead of imitating it

--- src/de/pizza/views/command/BestellerfassungAbgebrochenCommand.java
+++ src/de/pizza/views/command/BestellerfassungAbgebrochenCommand.java
@@ -1,24 +1,4 @@
 package de.pizza.views.command;
 
-import javax.swing.JFrame;
-
-import de.pizza.controller.Pizzeria;
-import de.pizza.views.Bestelluebersicht;
-import de.pizza.views.GuiHandler;
-
-public class BestellerfassungAbgebrochenCommand implements Command, MaskenErsteller {
-
-  public BestellerfassungAbgebrochenCommand() {
-  }
-
-  @Override
-  public void execute(Pizzeria pizzeria, CommandHandler commandHandler, GuiHandler guiHandler) {
-    guiHandler.wechseleZu(this, pizzeria, commandHandler);
-  }
-
-  @Override
-  public JFrame erstelleMaske(Pizzeria pizzeria, CommandHandler commandHandler) {
-    return new Bestelluebersicht(pizzeria, commandHandler);
-  }
-
+public class BestellerfassungAbgebrochenCommand extends ZurUebersichtGehenCommand {
 }
