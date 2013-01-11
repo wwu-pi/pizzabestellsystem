8f4d0bec3ac71a8569cc175507716c187bb24600
Jan Ernsting <jan.ernsting@gmail.com>
2013-01-11 11:02:48 +0100

    Add test for canceling orders

---
+++ src/de/pizza/views/command/BestellungStornierenTest.java
@@ -0,0 +1,31 @@
+package de.pizza.views.command;
+
+import org.junit.Test;
+
+import de.pizza.domain.Bestellung;
+import de.pizza.views.GuiHandler;
+
+import static org.mockito.Mockito.*;
+
+public class BestellungStornierenTest {
+
+  @Test
+  public void sollteBestellungStornieren() {
+    Bestellung bestellung = mock(Bestellung.class);
+    
+    new BestellungStornierenCommand(bestellung).execute(null, null, null);
+    
+    verify(bestellung).stornieren();
+  }
+  
+  @Test
+  public void sollteKeinenMaskenWechselAusloesen() {
+    GuiHandler guiHandler = mock(GuiHandler.class);
+    Bestellung bestellung = mock(Bestellung.class);
+     
+    new BestellungStornierenCommand(bestellung).execute(null, null, guiHandler);
+    
+    verifyZeroInteractions(guiHandler);
+  }
+
+}
