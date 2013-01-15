b16043aca71138d5016f87e1c3666f11b3973277
Jan Ernsting <jan.ernsting@gmail.com>
2013-01-15 13:45:38 +0100

    Explain purpose of Command and MaskenErsteller

--- src/de/pizza/views/command/CommandHandler.java
+++ src/de/pizza/views/command/CommandHandler.java
@@ -1,5 +1,10 @@
 package de.pizza.views.command;
 
+/**
+ * Die implementierende Klasse führt die übergebenen {@link Command} per
+ * {@link Command#execute(de.pizza.controller.Pizzeria, CommandHandler, de.pizza.views.GuiHandler)}
+ * aus
+ */
 public interface CommandHandler {
 
   void process(Command command);
--- src/de/pizza/views/command/MaskenErsteller.java
+++ src/de/pizza/views/command/MaskenErsteller.java
@@ -3,7 +3,18 @@ package de.pizza.views.command;
 import javax.swing.JFrame;
 
 import de.pizza.controller.Pizzeria;
+import de.pizza.views.GuiHandler;
 
+/**
+ * <p>Die implemtierende Klasse konstruiert eine Maske und liefert sie als
+ * {@link JFrame} zurück.</p>
+ * 
+ * <p>Durch die Entkopplung (Aufruf durch
+ * {@link GuiHandler#wechseleZu(MaskenErsteller, Pizzeria, CommandHandler)})
+ * werden die Masken nur zur Laufzeit und nicht unnötigerweise in den Tests
+ * erstellt. Wenn die Masken bereits in den Tests konstruiert werden, verlängert
+ * sich die Testausführung jeweils enorm (Bsp: von 0,003s auf ~0,6s).</p>
+ */
 public interface MaskenErsteller {
 
   public JFrame erstelleMaske(Pizzeria pizzeria, CommandHandler commandHandler);
